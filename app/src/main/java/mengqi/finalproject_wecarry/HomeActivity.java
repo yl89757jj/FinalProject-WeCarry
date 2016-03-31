8163cbebf7203866ff6e201f94dabf60c187b1ff
yl89757jj <yanglu930309@gmail.com>
2016-03-30 22:47:49 -0400

    with home page2

--- app/src/main/java/mengqi/finalproject_wecarry/HomeActivity.java
+++ app/src/main/java/mengqi/finalproject_wecarry/HomeActivity.java
@@ -1,13 +1,13 @@
 package mengqi.finalproject_wecarry;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home2);
+        setContentView(R.layout.activity_home);
     }
 }
